aca66151bfa15a3b8ab2e4a95afd80d0b38727af
dines07 <93317591+dines07@users.noreply.github.com>
2021-10-29 11:48:05 +0530

    forth commit

--- app/src/main/java/com/mple/dies/MainActivity.kt
+++ app/src/main/java/com/mple/dies/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
-        dajuess ads
+        today is a rainy day
     }
 
 }
